1c32971f1cd093e6706f3d5e868d77c5116ad4d6
Riguel Figueiro <f_riguel.figueiro@gauge.haus>
2025-05-16 16:00:44 -0300

    feature: added column to version control

---
+++ backend/Infrastructure/DataBase/Migrations/20250516183657_AddConcurrencyVersionColumnOnProduct.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.DataBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddConcurrencyVersionColumnOnProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "row_version",
+                table: "products",
+                type: "bytea",
+                rowVersion: true,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "row_version",
+                table: "products");
+        }
+    }
+}
